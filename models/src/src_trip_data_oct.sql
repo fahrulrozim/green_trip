849f19340a6654aeda87d08f34f3bb678d8e9cc6
Fahrul Rozi M <fahrulrozimeiduan@gmail.com>
2022-09-29 09:49:53 +0000

    add sources table database

---
+++ models/src/src_trip_data_oct.sql
@@ -0,0 +1,20 @@
+WITH green_taxi_trip_data_oct AS (
+    SELECT
+        *
+    FROM
+       {{ source('green_taxi_trip', 'trip_data_oct') }}
+)
+SELECT
+    VendorID AS vendor_id,
+    lpep_pickup_datetime AS pickup_datetime,
+    lpep_dropoff_datetime AS dropoff_datetime,
+    store_and_fwd_flag,
+    RatecodeID AS ratecode_id,
+    PULocationID AS pu_location_id,
+    DOLocationID AS do_location_id,
+    passenger_count,
+    trip_distance,
+    payment_type,	
+    trip_type
+FROM
+    green_taxi_trip_data_oct
